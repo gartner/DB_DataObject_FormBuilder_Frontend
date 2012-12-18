62ec70d02c6000140d17e213f28ff7c83a196466
Mads Lie Jensen <mads@gartneriet.dk>
2012-12-18 07:32:20 +0100

    Add: mysqli-driver for the DB-layer of the DB_DataObject_Frontend_FormBuilder_Util_DBQuery part.

---
+++ src/DB/DataObject/FormBuilder/Frontend/Util/DbQuery/DB/Mysqli.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Mads
+ * Date: 17-12-12
+ * Time: 09:04
+ * To change this template use File | Settings | File Templates.
+ */
+require_once 'DB/DataObject/FormBuilder/Frontend/Util/DbQuery/DB/Mysql.php';
+
+class DB_DataObject_FormBuilder_Frontend_Util_DbQuery_DB_Mysqli extends
+    DB_DataObject_FormBuilder_Frontend_Util_DbQuery_DB_Mysql
+{}
