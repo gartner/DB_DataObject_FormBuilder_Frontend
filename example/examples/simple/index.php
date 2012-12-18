eabb7d4e52d87a6482a9623e9873387b893dd543
Mads Lie Jensen <mads@gartneriet.dk>
2012-12-18 08:27:03 +0100

    Add: Show source/config-links to all examples.

--- example/examples/simple/index.php
+++ example/examples/simple/index.php
@@ -18,36 +18,45 @@ DB_DataObject_FormBuilder_Frontend-package.</p>
 <p>The most basic way to use the frontend. Using the Renderer to manage it.</p>
 <p>It sets up some default values, and defines which columns from the table to
 display in the table list.</p>
+<p>
+    [ <a href="/examples/showExampleSource.php?example=/simple/1/&amp;type=xml">Show configfile</a> ]
+    [ <a href="/examples/showExampleSource.php?example=/simple/1/&amp;type=php">Show source</a> ]
+</p>
 
 <h3><a href="2/">Example 2</a></h3>
 <p>Just like the above example, except, this one will include a css-file in the
 generated page. This makes the page look so much nicer.</p>
 
+<p>
+    [ <a href="/examples/showExampleSource.php?example=/simple/2/&amp;type=xml">Show configfile</a> ]
+    [ <a href="/examples/showExampleSource.php?example=/simple/2/&amp;type=php">Show source</a> ]
+</p>
+
 <h3><a href="3/">Example 3</a></h3>
 <p>Like example 2, but instead of using the Renderer, use the Frontend more direct.</p>
 
 <script type="syntaxhighlighter" class="brush: php"><![CDATA[
-&lt;?php
-$frontend = new DB_DataObject_FormBuilder_Frontend('config.xml', true);
+    &lt;?php
+    $frontend = new DB_DataObject_FormBuilder_Frontend('config.xml', true);
 
-$output = $frontend->process();
+    $output = $frontend->process();
 
-if (!empty($frontend->css){
-    echo '<link rel="stylesheet" type="text/css" href="' . $frontend->css} . '"/>';
-}
+    if (!empty($frontend->css){
+        echo '<link rel="stylesheet" type="text/css" href="' . $frontend->css} . '"/>';
+    }
 
-echo $frontend->getToolbar();
+    echo $frontend->getToolbar();
 
-echo $output;
-?>
+    echo $output;
+    ?>
 ]]></script>
 
 <p>When doing that, you have to do several things. First, check if there is a css-file from the config that should be added to the page. (Line 6-8)</p>
 <p>Then print the toolbar (->getToolbar()) The toolbar is the one containing the 'add new record'-link and the pager. (Line 10)</p>
 <p>Finally, output whatever is returned from ->process() (Line 12). But, since ->process() might do a http-redirect, it needs to be called before outputting anything. (Line 4)</p>
 <p>
-    [ <a href="/examples/showExampleSource.php?example=/simple/1/&amp;type=xml">Show configfile</a> ]
-    [ <a href="/examples/showExampleSource.php?example=/simple/1/&amp;type=php">Show configfile</a> ]
+    [ <a href="/examples/showExampleSource.php?example=/simple/3/&amp;type=xml">Show configfile</a> ]
+    [ <a href="/examples/showExampleSource.php?example=/simple/3/&amp;type=php">Show source</a> ]
 </p>
 <?php
 require_once 'foot.php';
